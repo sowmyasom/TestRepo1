9dca50437b0119b6ff263dc901be248eca5db814
minion <sowmyasomi9179@gmail.com>
2021-06-30 11:54:42 +0530

    print statement added

--- things.py
+++ things.py
@@ -12,8 +12,6 @@ while True:
 		response = urllib2.urlopen(
 		'https://api.thingspeak.com/update?api_key=XI5ZVSAPPCCKWUTG&field1='+message.split(": ")[1][:-2])
 		html = response.read()
-		print("hellooooooo saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-		current  =  datetime.datetime.now()
-		print(current)
 		time.sleep(20)
-		
+		print("hi likitha")
+
